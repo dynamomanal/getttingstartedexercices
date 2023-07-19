e7a3fc06f22341a1b5e8093aa924518584b1706a
Manal Rana <133088089+dynamomanal@users.noreply.github.com>
2023-07-19 21:52:46 +0500

    Add files via upload

---
+++ q3.js
@@ -0,0 +1,5 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.//
+var author = "Martin Fowler";
+var quote = "Any fool can write a code that a computer can understand. Good programmers write code that humans can understand.";
+console.log("".concat(author, " once said,\" ").concat(quote, "\""));
---
+++ q3.ts
@@ -0,0 +1,6 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.//
+let author:string= "Martin Fowler";
+let quote:string="Any fool can write a code that a computer can understand. Good programmers write code that humans can understand.";
+console.log(`${author} once said," ${quote}"`);
+/*Any fool can write code that a computer can understand. Good programmers write code that humans can understand.*/
